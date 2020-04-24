79426586b7fad4d85332d9ce7a6767008e0c104e
Ebaa Abdelfadeel <ebaa.abdelfadeel@gmail.com>
2020-04-24 22:02:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    10 * "s"  + string
   else
     string
   end
 end
+
+snake_it_up("string")
